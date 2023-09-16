1440be22a673106920b67e85afdafb89679163e7
Florencia Gomez <gomez-mf@hotmail.com>
2023-09-16 17:08:03 -0300

    Se crean componentes toolbar, navbar y students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StudentsComponent } from './students/students.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavBarComponent,
+    StudentsComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
